719d2989bf1f94a57860960a814809a7bfe9e5eb
colby-campbell <colbycampbell8731@gmail.com>
2025-11-23 15:34:16 -0700

    Feat: Added extra functions to make formatting logs more efficient

--- format_helper.py
+++ format_helper.py
@@ -1,5 +1,19 @@
 from datetime import datetime
+import enum
 import re
+from webbrowser import get
+
+# Constants
+SPACING_LENGTH = 9
+
+class LogLevel(enum.Enum):
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    DEBUG = "DEBUG"
+    RAW = "RAW"
+    CLI = "CLI"
+    CRITICAL = "CRITICAL"
 
 
 def get_timestamp():
@@ -7,8 +21,36 @@ def get_timestamp():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f":{datetime.now().microsecond // 1000:03d}"
 
 
+def get_spacing(level: LogLevel):
+    """
+    Get spacing for log level alignment.
+    Args:
+        level (LogLevel): The log level.
+    Returns:
+        str: The spacing string.
+    """
+    return " " * (max(SPACING_LENGTH - len(level.value), 1))
+
+
+def get_prefix(level: LogLevel):
+    """
+    Get the formatted prefix for a given log level.
+    Args:
+        level (LogLevel): The log level.
+    Returns:
+        str: The formatted prefix string.
+    """
+    return f"{get_timestamp()} {level.value}{get_spacing(level)}"
+
+
 def process_line(line):
-    """Process a line from the server."""
+    """
+    Process a line from the server.
+    Args:
+        line (str): The line to process.
+    Returns:
+        list[str]: A list containing the formatted prefix and the message.
+    """
     # Regex to parse log lines
     pattern = re.compile(r"\[(?P<timestamp>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}[:,]\d{3}) (?P<level>\w+)\](?: (?P<message>.*))?")
     match = pattern.match(line)
@@ -18,11 +60,11 @@ def process_line(line):
         timestamp = timestamp.replace(",", ":")
         level = match.group("level")
         # Calculate spacing for alignment
-        spacing = " " * (max(9 - len(level), 1))
+        spacing = " " * (max(SPACING_LENGTH - len(level), 1))
         # Get the message part ('or ""' to handle None case)
         message = match.group("message") or ""
         # Return the formatted line
         return [f"{timestamp} {level}{spacing}", message]
     else:
         # If the line doesn't contain a timestamp, return it with an 'RAW' level
-        return [f"{get_timestamp()} RAW      ", line]
+        return [f"{get_timestamp()} RAW{get_spacing(LogLevel.RAW)}", line]
